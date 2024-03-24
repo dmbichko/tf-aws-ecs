a893485fb6337076b2158df6d94999ae035dce21
dmbichko <dmitrijbichko@gmail.com>
2024-03-24 21:33:00 +0300

    add public route table

--- vpc.tf
+++ vpc.tf
@@ -16,7 +16,6 @@ resource "aws_subnet" "public" {
   availability_zone       = local.azs_names[count.index]
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 10 + count.index)
   map_public_ip_on_launch = true
-
   tags                    = { Name = "public-${local.azs_names[count.index]}" }
 }
 
@@ -29,9 +28,27 @@ resource "aws_internet_gateway" "main" {
     tomap({ "Name" = "${local.prefix}-igw" })
   )
 }
-
 resource "aws_eip" "main" {
   count      = local.azs_count
   depends_on = [aws_internet_gateway.main]
-  tags       = { Name = "demo-eip-${local.azs_names[count.index]}" }
+  tags       = { Name = "eip-${local.azs_names[count.index]}" }
+}
+# --- Public Route Table ---
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-igw" })
+  )
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = local.azs_count
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
