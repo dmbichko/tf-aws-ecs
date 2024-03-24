f6d10588aabfdb5656df69bd0576ac00db91e3e7
dmbichko <dmitrijbichko@gmail.com>
2024-03-24 21:25:38 +0300

    add gitignore and igw

--- vpc.tf
+++ vpc.tf
@@ -19,3 +19,19 @@ resource "aws_subnet" "public" {
 
   tags                    = { Name = "public-${local.azs_names[count.index]}" }
 }
+
+# --- Internet Gateway ---
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-igw" })
+  )
+}
+
+resource "aws_eip" "main" {
+  count      = local.azs_count
+  depends_on = [aws_internet_gateway.main]
+  tags       = { Name = "demo-eip-${local.azs_names[count.index]}" }
+}
